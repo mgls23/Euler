423ba5151f048979526b05b0edcc54b39f4f16cb
dyxogus <taehyunlee12@gmail.com>
2016-10-15 17:56:27 +0100

    Small Cleanup

--- name-scores/main.py
+++ name-scores/main.py
@@ -9,12 +9,14 @@ def run():
 
 	print cumulative
 
+
 base = ord('A') - 1
+
+
 def calculate_score(name):
 	cumulative = sum([ord(char) for char in name])
 	return cumulative - (base * len(name))
 
+
 if __name__ == '__main__':
 	run()
-	print ord('C')
-	print ord('A')
