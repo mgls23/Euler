c9c20a9e0edebc6e3937fbf88d936fe5b7467521
Taehyun Lee <taehyunlee12@gmail.com>
2016-12-16 00:51:30 +0900

    Coding Standards Implemented

--- README.md
+++ README.md
@@ -4,3 +4,15 @@ challenges.
 
 See more Info at ::
   https://projecteuler.net/archives
+
+# Coding Standard
+
+Not a hard limit by any means, but it should give a good idea
+
+* PEP8, PEP8, PEP8
+* Asserts, lots of them
+* Right Margin [76]
+* Spaces [4]
+* Function Line [10]
+* Objects when possible
+* Descriptive variable names
--- main.py
+++ main.py
@@ -6,158 +6,165 @@ from const import problemArgs, ANSWERS
 
 
 # Q3 :: Largest Prime Factor
-def find_primes(x=600851475143):
-	"""
-		Finds prime factors* of a given number
-		* prime factors :: prime numbers that make up the given number once multiplied together
+def find_primes(n=600851475143):
+    """
+    Finds prime factors* of a given number
+        * prime factors :: prime numbers that make up the given number
+                           once multiplied together
 
-		Parameters
-		----------
-			x: int
-				the number to conduct the search on primes on
+        Args
+        ----
+            n: int
+                the number to conduct the search on primes on
 
-		Returns
-		-------
-			prime_factors: [int]
-				prime numbers that make up the
-	"""
-	prime_factors, prime_iterator, prime_number, x_copy = [], prime.PrimeGenerator(), 1, x
+        Returns
+        -------
+            prime_factors: [int]
+                prime numbers that make up the
+    """
+    prime_factors = []
+    prime_iterator = prime.PrimeGenerator()
+    prime_number = 1
+    x_copy = n
 
-	while x_copy > 1 and prime_number < x:
-		prime_number = prime_iterator.next()
-		if x_copy % prime_number == 0:
-			x_copy /= prime_number
-			prime_factors.append(prime_number)
+    while x_copy > 1 and prime_number < n:
+        prime_number = prime_iterator.next()
+        if x_copy % prime_number == 0:
+            x_copy /= prime_number
+            prime_factors.append(prime_number)
 
-	return max(prime_factors)
+    return max(prime_factors)
 
 
 # Q6 :: Sum Square Difference
 def square_difference(n=100):
-	"""
-	(1 + 2 + 3 + ... n)^2
-	 = ((n * (n + 1)) / 2) ^ 2
+    """
+    (1 + 2 + 3 + ... n)^2
+     = ((n * (n + 1)) / 2) ^ 2
 
-	1^2 + 2^2 + 3^2 + ... n^2
+    1^2 + 2^2 + 3^2 + ... n^2
      [BRUTE FORCE]
 
-	Args
-	----
-		n: int
+        Args
+        ----
+            n: int
 
-	Returns
-	-------
+        Returns
+        -------
 
-	"""
-	square_of_sum = ((n + 1) * n / 2) ** 2
-	sum_of_square = sum([i ** 2 for i in range(n + 1)])
-	return square_of_sum - sum_of_square
+    """
+    square_of_sum = ((n + 1) * n / 2) ** 2
+    sum_of_square = sum([i ** 2 for i in range(n + 1)])
+    return square_of_sum - sum_of_square
 
 
 # Q7 :: 10001st prime
 def q_10001th_prime(n=10001):
-	return prime.PrimeGenerator().generate_nth(n)
+    return prime.PrimeGenerator().generate_nth(n)
 
 
 # Q8 :: Adjacent Multiplicand
-def adjacent(x, window_size=13):
-	"""
+def adjacent_multiplicand(x, window_size=13):
+    """
 
-	Parameters
-	----------
-	x
-	window_size
+    Args
+    ----
+        x
+        window_size
 
-	Returns
-	-------
+    Returns
+    -------
 
-	"""
-	assert x and window_size, "Please provide a number that we could run some cool maths on"
+    """
+    assert x and window_size, "Please provide a number that we could run " \
+                              "some cool maths on"
 
-	# Break down x into a list
-	x = [int(strx) for strx in (type(x) == int) and str(x) or x]
-	y = x[:-1]
-	# z = len(x) - window_size
-	for iteration_count in range(1, window_size):
-		for index in range(len(y) - 1):
-			y[index] *= x[index + iteration_count]
+    # Break down x into a list
+    x = [int(strx) for strx in (type(x) == int) and str(x) or x]
+    y = x[:-1]
 
-		y.pop(-1)
+    for iteration_count in range(1, window_size):
+        for index in range(len(y) - 1):
+            y[index] *= x[index + iteration_count]
 
-	# if iteration_count == z:
-	# 	break
+        y.pop(-1)
 
-	return max(y)
+    return max(y)
 
 
 # Q16 :: Digit of 2^1000
 def power_digit_sum(power=15):
-	"""
-	1, 2, 4, 8, 16, 32, 64, 128, ...
+    """
+    1, 2, 4, 8, 16, 32, 64, 128, ...
 
 
-	Parameters
-	----------
-	power
+        Args
+        ----
+            power
 
-	Returns
-	-------
+        Returns
+        -------
+    """
+    assert power > 0, "Please provide a number bigger than 0 " \
+                      "so we can do some cool maths"
 
-	"""
-	assert power > 0, "Please provide a cool number we can run some maths on"
+    number_array_repr = [1]
+    for i in range(power):
 
-	number_array_repr = [1]
-	for i in range(power):
+        for j in range(len(number_array_repr)):
+            number_array_repr[j] *= 2
 
-		for j in range(len(number_array_repr)):
-			number_array_repr[j] *= 2
+        for index in range(len(number_array_repr)):
+            while number_array_repr[index] >= 10:
+                number_array_repr[index] -= 10
+                try:
+                    number_array_repr[index + 1] += 1
 
-		for index in range(len(number_array_repr)):
-			while number_array_repr[index] >= 10:
-				number_array_repr[index] -= 10
-				try:
-					number_array_repr[index + 1] += 1
-				except IndexError:
-					number_array_repr.append(1)
+                except IndexError:
+                    number_array_repr.append(1)
 
-	return sum(number_array_repr)
+    return sum(number_array_repr)
 
 
 problemHandler = \
-	{
-		# 1: fizz_buzz,
+    {
+        # 1: fizz_buzz,
+        3: find_primes,
 
-		5: smallest_multiple,
-		6: square_difference,
-		7: q_10001th_prime,
-		8: adjacent,
+        5: smallest_multiple,
+        6: square_difference,
+        7: q_10001th_prime,
+        8: adjacent_multiplicand,
 
-		16: power_digit_sum,
+        16: power_digit_sum,
+    }
 
-		23: non_abundant_sum,
-	}
+
+def run():
+    # Configure Parser + Parse
+    parser = argparse.ArgumentParser(description='Project Euler')
+    parser.add_argument(
+        'n', type=int, help='The Problem Number'
+        'p', type=
+    )
+    args = parser.parse_args()
+
+    # Hack
+    index = args.n
+
+    # Execute Problem
+    result = problemHandler[index](**problemArgs[index])
+
+    try:
+        answer = ANSWERS[index]
+        assert (result == answer), \
+            'Result for Problem %(index)s is %(result)s ' \
+            'and does not match the expect, which is %(answer)s' % locals()
+
+    except KeyError:
+        print 'This problem has not been solved yet'
+        print 'Output [for problem %(index)s] is %(result)s' % locals()
+
 
 if __name__ == '__main__':
-	# Configure Parser + Parse
-	parser = argparse.ArgumentParser(
-		description='Project Euler, dyxogus implementation')
-	parser.add_argument('n', metavar='n', type=int,
-	                    help='The Problem that needs to be solved')
-	args = parser.parse_args()
-
-	# Hack
-	index = args.n
-	index = 23
-
-	# Execute Problem
-	computed = problemHandler[index](**problemArgs[index])
-
-	try:
-		expected = ANSWERS[index]
-		assert (computed == expected), \
-			'Computed Answer for Problem %(index)s is %(computed)s ' \
-			'and does not match the expect, which is %(expected)s' % locals()
-
-	except KeyError:
-		print 'This problem has not been solved yet'
-		print 'Computed Answer for Problem %(index)s is %(computed)s' % locals()
+    run()
