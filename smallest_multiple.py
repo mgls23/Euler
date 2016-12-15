243a22d062fffb2fe974dfa24f1322359cbc1764
Taehyun Lee <taehyunlee12@gmail.com>
2016-12-16 02:16:49 +0900

    Small Fix + Refactoring for Smallest Multiple

--- smallest_multiple.py
+++ smallest_multiple.py
@@ -1,50 +1,61 @@
-import sys
-
 from common import prime
 
 
-def foo(x):
-	if x <= 1:
-		return {}
+def cumulative_lcm_in_prime_powers(n):
+    if n <= 1:
+        return {}
 
-	prime_dictionaries = []
-	primes = prime.PrimeGenerator().generate_to(x)
+    all_prime_factors = {}
+    primes = prime.PrimeGenerator().generate_to(n)
 
-	for index in range(primes[0], x):
-		prime_dictionaries.append(generate_prime_dictionary(index, primes))
+    for number in range(primes[0], n):
+        prime_powers = decompose_to_prime_powers(number, primes)
 
-	prime_dict = {}
-	for prime_dictionary in prime_dictionaries:
-		for key, value in prime_dictionary.iteritems():
-			prime_dict[key] = max(prime_dict.get(key, -sys.maxint), value)
+        for prime_number, power in prime_powers.iteritems():
+            all_prime_factors[prime_number] = \
+                max(all_prime_factors.get(prime_number, 0), power)
 
-	return prime_dict
-
-
-def generate_prime_dictionary(something, primes):
-	prime_dictionary = {}
-	for prime in primes:
-		while not something % prime:
-			if prime_dictionary.has_key(prime):
-				prime_dictionary[prime] += 1  #
-			else:
-				prime_dictionary[prime] = 1
-
-			something /= prime
-			if something == 1:
-				return prime_dictionary
-
-	return prime_dictionary
+    return all_prime_factors
 
 
-def bar(prime_dict):
-	cumulative = 1
-	for key, value in prime_dict.iteritems():
-		cumulative *= key ** value
+def decompose_to_prime_powers(number, primes):
+    prime_composition = {}
+    for prime_number in primes:
+        while not number % prime_number:
+            if prime_composition.has_key(prime_number):
+                prime_composition[prime_number] += 1
 
-	return cumulative
+            else:
+                prime_composition[prime_number] = 1
+
+            number /= prime_number
+            if number == 1:
+                return prime_composition
+
+    assert number == 1, "Bad Programmer Error"
 
 
-def smallest_multiple(n):
-	return bar(foo(n))
+def pi_of_factors_with_power(primes_composition):
+    """ Finds the multiplicative sum [pi] of factors with indicated powers
 
+    Args
+    ----
+        primes_composition:
+            {
+                N1: a,
+                N2: b,
+                N3: c,
+                ...
+            }
+    Returns
+    -------
+        (N1 ^ a) * (N2 ^ b) * ...
+    """
+    return reduce(
+        lambda x, y: x * y,
+        [key ** value for key, value in primes_composition.iteritems()]
+    )
+
+
+def smallest_multiple_up_to(n):
+    return pi_of_factors_with_power(cumulative_lcm_in_prime_powers(n))
