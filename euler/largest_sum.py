9d148d5655afeaa0e112317b97cb0dee9f842167
Taehyun Lee <taehyunlee12@gmail.com>
2017-06-10 15:43:11 +0100

    Master: 13 fixed

--- euler/largest_sum.py
+++ euler/largest_sum.py
@@ -1,47 +1,26 @@
-import logging
+import math
 
 
-def first_n_digits_of_sum(first_digits, numbers, desired_type=str):
+def first_n_digits_of_sum(first_digits, numbers, output_type=int):
+    """Assumes that the length of numbers are equal
+
+    :param first_digits:int
+    :param numbers: list 
+    :param output_type: type 
     """
-    Assumes that the lengh of numbers are equal
+    accumulates = []
+    carry = 0
 
-    :param first_digits:
-    :param numbers:
-    :param desired_type:
-    """
-    final_accumulated_in_reverse = []
-    current_sum = 0
+    for string_index in range(len(numbers[0]) + 1):
+        accumulate = sum(int(number[-string_index]) for number in numbers) + carry
+        carry = math.floor(accumulate / 10)
+        accumulates.append(accumulate % 10)
 
-    for index in range(len(numbers[0]) - 1, -1, -1):
-        for number in numbers:
-            logging.debug('Before :: current_sum=%(current_sum)s' % locals())
-            logging.debug('index  :: {}'.format(number[index]))
-            current_sum += int(number[index])
-            logging.debug('After  :: current_sum=%(current_sum)s' % locals())
+    carry = int(carry)
+    while carry > 0:
+        accumulates.append(carry % 10)
+        carry = math.floor(carry / 10)
 
-        if current_sum != 0:
-            current_digit = current_sum % 10
-            logging.debug(
-                'Before :: ' \
-                'current_digit=%(current_digit)s, ' \
-                'current_sum=%(current_sum)s' % locals()
-            )
-            final_accumulated_in_reverse.append(str(current_digit))
-
-        current_sum /= 10
-        logging.debug('After  :: current_digit=%(current_digit)s, ' \
-            'current_sum=%(current_sum)s' % locals()
-        )
-
-    remaining = list(str(current_sum))
-    logging.debug('current_sum=%(current_sum)s, ' \
-                  'remaining=%(remaining)s' % locals())
-
-    if len(remaining) > 0 and current_sum > 0:
-        remaining.reverse()
-        final_accumulated_in_reverse += remaining
-
-    final_accumulated_in_reverse.reverse()
-    final_sum = ''.join(final_accumulated_in_reverse[:first_digits])
-
-    return desired_type(final_sum)
+    accumulates.reverse()
+    final_sum = ''.join(map(str, accumulates))
+    return output_type(final_sum[:first_digits])
