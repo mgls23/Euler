ccc379070e89bf9aa498ebab9567642565470d75
Taehyun Lee <taehyunlee12@gmail.com>
2017-06-13 21:00:20 +0100

    4: Solution Added

---
+++ euler/largest_palindrome_product.py
@@ -0,0 +1,53 @@
+import math
+
+
+def all_range(digit):
+    for x in range(10 ** digit, 10 ** (digit - 1), -1):
+        for y in range(10 ** digit - 1, 10 ** (digit - 1), -1):
+            yield x * y
+
+
+def find_largest_palindrome(digit):
+    return max(filter(is_palindrome, all_range(digit)))
+
+
+def largest_palindrome(digit):
+    max_palindrome = -1
+    for x in range(10 ** digit, 10 ** (digit - 1), -1):
+        for y in range(10 ** digit - 1, 10 ** (digit - 1), -1):
+            number = x * y
+            if is_palindrome(number):
+                max_palindrome = max(max_palindrome, number)
+
+    return max_palindrome
+
+
+def is_palindrome(number):
+    number_string = str(number)
+    half = int(math.ceil(len(number_string) / 2))
+    for index in range(half):
+        if number_string[index] != number_string[-index - 1]:
+            return False
+
+    return True
+
+
+# def test_is_palindrome():
+#     is_palindrome_check = {
+#         1: True,
+#         10: False,
+#         11: True,
+#         121: True,
+#         1231: False,
+#     }
+#
+#     for number, is_palindrome_ in is_palindrome_check.items():
+#         assert is_palindrome_ == is_palindrome(number), \
+#             "{} is not {}".format(number, is_palindrome_)
+
+
+if __name__ == '__main__':
+    # test_is_palindrome()
+    # print(largest_palindrome(3))
+    print(find_largest_palindrome(3))
+#
