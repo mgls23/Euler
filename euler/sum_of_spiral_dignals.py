887453320bf424b5c449a3fbcd851f7e45e7c59a
Taehyun Lee <taehyunlee12@gmail.com>
2019-10-14 19:23:37 +0100

    Added loose files

---
+++ euler/sum_of_spiral_dignals.py
@@ -0,0 +1,10 @@
+def spam():
+    return -1
+
+
+def q28():
+    return spam()
+
+
+if __name__ == '__main__':
+    print(q28())
---
+++ euler/summation_of_primes.py
@@ -0,0 +1,5 @@
+from euler.util import prime
+
+upper_limit = 2000000
+
+print(prime.prime_numbers_smaller_than(upper_limit))
