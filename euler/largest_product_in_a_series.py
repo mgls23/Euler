29d1fcc9bb450b837cb6d1259f980f7c6e5d6879
Taehyun Lee <taehyunlee12@gmail.com>
2019-10-23 18:46:48 +0100

    11 Simple solution added (lazy)

--- euler/largest_product_in_a_series.py
+++ euler/largest_product_in_a_series.py
@@ -1,18 +1,29 @@
-def adjacent_multiplicand(string, window_size):
-    """
-    :param string :str
-    :param window_size :int
-    """
-    assert string and window_size
+import numpy
 
-    # Break down x into a list
-    string = list(map(int, string))
-    y = string[:-1]
+
+def adjacent_multiplicand_string(input_string, window_size):
+    return adjacent_multiplicand(list(map(int, input_string)), window_size)
+
+
+def adjacent_multiplicand(list_, window_size):
+    y = list_[:-1]
 
     for iteration_count in range(1, window_size):
         for index in range(len(y) - 1):
-            y[index] *= string[index + iteration_count]
+            y[index] *= list_[index + iteration_count]
 
         y.pop(-1)
 
     return max(y)
+
+
+def horizontal(matrix):
+    return numpy.array(matrix).transpose().tolist()
+
+
+def left_diagonal(matrix):
+    return numpy.array([[0] * i + row + [0] * (len(matrix) - i) for i, row in enumerate(matrix)]).transpose().tolist()
+
+
+def right_diagonal(matrix):
+    return numpy.array([[0] * (len(matrix) - i) + row + [0] * i for i, row in enumerate(matrix)]).transpose().tolist()
