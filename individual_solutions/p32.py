ec8f703301de7d610022b1c46be5bc8b15fa32a3
Taehyun Lee <taehyunlee12@gmail.com>
2020-02-29 14:13:35 +0000

    [32] Simplify + speedup by exploiting LHS is always 5 digits

--- individual_solutions/p32.py
+++ individual_solutions/p32.py
@@ -1,37 +1,25 @@
-from itertools import combinations
-
 from euler.util.decorators import timed_function
 
 
-def is_partial_pandigital(number):
-    number_string = str(number)
+def is_partially_1_to_9_pandigital(number_string):
     return len(set(number_string) - {'0'}) == len(number_string)
 
 
-def is_pandigital_product(x, y):
-    string_x, string_y = str(x), str(y)
-    unique_numbers = set(string_x) | set(string_y)
-    if len(unique_numbers) == len(string_x) + len(string_y):  # duplicating checks for worthwhile speedup
-        product_string = str(x * y)
-        unique_numbers |= set(product_string)
-        unique_numbers.discard('0')
-        return len(unique_numbers) == 9 == len(string_x) + len(string_y) + len(product_string)
-
-
 def q32():
+    total_left_digits = 5  # Only consider 5 digit numbers on LHS
     pan_digitals = set()
 
-    for x, y in combinations(list(filter(is_partial_pandigital, range(10 ** 3))), 2):
-        if is_pandigital_product(x, y):
-            pan_digitals.add(x * y)
+    numbers_in_range = range(10 ** (total_left_digits - 1), 10 ** total_left_digits)
+    for left_hand_side in filter(is_partially_1_to_9_pandigital, map(str, numbers_in_range)):
+        for split_index in range(0 + 1, total_left_digits - 1):
+            multiplicand, multiplier = left_hand_side[:split_index], left_hand_side[split_index:]
+            product_string = str(int(multiplicand) * int(multiplier))
+            unique_numbers = set(left_hand_side) | set(product_string) - {'0'}
 
-    # handle 1 digit x 4 digit
-    for y in filter(is_partial_pandigital, range(10 ** 4)):
-        for x in range(1, 10):
-            if is_pandigital_product(x, y):
-                pan_digitals.add(x * y)
+            if len(unique_numbers) == 9 == len(left_hand_side) + len(product_string):
+                pan_digitals.add(product_string)
 
-    return sum(pan_digitals)
+    return sum(map(int, pan_digitals))
 
 
 if __name__ == '__main__':
