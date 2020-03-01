12a4202e9b6f6966ec28056a051edc366f8eac15
Taehyun Lee <taehyunlee12@gmail.com>
2020-03-01 19:50:39 +0000

    [53] Optimisation through observation

--- individual_solutions/p53.py
+++ individual_solutions/p53.py
@@ -3,8 +3,8 @@ from math import factorial
 from euler.util.decorators import timed_function
 
 
-def find_first_non_fitting_r(n, ncr_needs_to_be):
-    starting_r = n // 2
+# If this extended (such that ncr_requirement can be smaller than n, this function will need to be modified
+def find_first_non_fitting_r(n, ncr_needs_to_be, starting_r):
     top = factorial(n) // factorial(n - starting_r)  # Special optimisation in factorial is fast
     bottom = factorial(starting_r)
     ncr = top // bottom
@@ -19,15 +19,15 @@ def find_first_non_fitting_r(n, ncr_needs_to_be):
 
 def q53(upper_range=100, ncr_requirement=10 ** 6):
     fitting_ncr_combinations = 0
+    r_boundary = 23 // 2 - 1
 
     # question stated first instance of n is 23
     for n in range(23, upper_range + 1):
-        non_fitting_r = find_first_non_fitting_r(n, ncr_needs_to_be=ncr_requirement)
+        r_boundary = find_first_non_fitting_r(n, ncr_requirement, r_boundary + 1)
 
         # No need to check if non_fitting_r is != n // 2
-        fitting_ncr_combinations += (n // 2 - non_fitting_r) * 2
-        if n % 2 == 0:
-            fitting_ncr_combinations -= 1
+        fitting_ncr_combinations += (n // 2 - r_boundary) * 2
+        if n % 2 == 0: fitting_ncr_combinations -= 1
 
     return fitting_ncr_combinations
 
