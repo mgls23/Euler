303a69fbe90c9334b4efc6d5aa09a107bbd40500
Taehyun Lee <taehyunlee12@gmail.com>
2020-03-01 18:37:18 +0000

    [52] Only start from 100000

--- individual_solutions/p52.py
+++ individual_solutions/p52.py
@@ -2,7 +2,8 @@ from euler.util.decorators import timed_function
 
 
 def q52(consecutive=6):
-    for number1_digit in range(1, 100000):
+    # 1 * 6 = 6 :: minimum is 100,000
+    for number1_digit in range(5, 100000):
         # 2 * 6 = 12 => increases digit. First digit *has* to be 1
         for number1 in range(10 ** number1_digit, 2 * 10 ** number1_digit):
             number1_string_set = set(str(number1))
