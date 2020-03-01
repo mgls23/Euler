619893b84515b023309e8a49e2ba6e049f30c6b8
Taehyun Lee <taehyunlee12@gmail.com>
2020-03-01 19:41:16 +0000

    [53] Refactoring

--- individual_solutions/p53.py
+++ individual_solutions/p53.py
@@ -3,33 +3,33 @@ from math import factorial
 from euler.util.decorators import timed_function
 
 
-def c(n, r):
-    return factorial(n) / factorial(r) / factorial(n - r)
+def find_first_non_fitting_r(n, ncr_needs_to_be):
+    starting_r = n // 2
+    top = factorial(n) // factorial(n - starting_r)  # Special optimisation in factorial is fast
+    bottom = factorial(starting_r)
+    ncr = top // bottom
+
+    for r in range(starting_r, 0, -1):
+        if ncr < ncr_needs_to_be:
+            return r
+
+        ncr *= r
+        ncr /= (n - r + 1)
 
 
-def q53(upper_range=100):
-    ncr_count = 0
+def q53(upper_range=100, ncr_requirement=10 ** 6):
+    fitting_ncr_combinations = 0
 
     # question stated first instance of n is 23
     for n in range(23, upper_range + 1):
-        r = n // 2
-        top = factorial(n) // factorial(n - r)  # Special optimisation in factorial is fast
-        bottom = factorial(r)
-        ncr = top // bottom
+        non_fitting_r = find_first_non_fitting_r(n, ncr_needs_to_be=ncr_requirement)
 
-        for r in range(r, 0, -1):
-            if ncr < 10 ** 6: break
+        # No need to check if non_fitting_r is != n // 2
+        fitting_ncr_combinations += (n // 2 - non_fitting_r) * 2
+        if n % 2 == 0:
+            fitting_ncr_combinations -= 1
 
-            ncr *= r
-            ncr /= (n - r + 1)
-
-        if r != n:
-            if n % 2 == 0:
-                ncr_count += 1 + (n // 2 - r - 1) * 2
-            else:
-                ncr_count += (n // 2 - r) * 2
-
-    return ncr_count
+    return fitting_ncr_combinations
 
 
 if __name__ == '__main__':
