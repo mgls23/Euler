ef82dcedbe9f250dfaf6a5eb1d801afa47abeaa9
Taehyun Lee <taehyunlee12@gmail.com>
2020-03-01 20:41:50 +0000

    [55] Solution Added

---
+++ individual_solutions/p55.py
@@ -0,0 +1,19 @@
+from euler.maths.palindromes import is_palindrome_simple_string
+from euler.util.decorators import timed_function
+
+
+def is_lychrel_number(number):
+    for iteration_count in range(50):
+        number += int(str(number)[::-1])
+        if is_palindrome_simple_string(str(number)): return False
+
+    return True
+
+
+def q55(upper_range=10 ** 4):
+    return len([number for number in range(upper_range) if is_lychrel_number(number)])
+
+
+if __name__ == '__main__':
+    assert (not timed_function(is_lychrel_number)(349))
+    assert (timed_function(q55)() == 249)
