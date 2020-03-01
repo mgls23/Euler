6c826fb8f351fc1f1b7246e841438f57afee6845
Taehyun Lee <taehyunlee12@gmail.com>
2020-03-01 20:03:13 +0000

    [53] Added note for potential optional retry

--- individual_solutions/p53.py
+++ individual_solutions/p53.py
@@ -17,6 +17,9 @@ def find_first_non_fitting_r(n, ncr_needs_to_be, starting_r):
         ncr /= (n - r + 1)
 
 
+# Optional :: Implement this in Pascal's Triangle.
+#   This is "better" version of Pascal's Triangle which uses symmetry to cut calculation by half
+#   and uses less memory - Pascal's Triangle is simpler. Also - I could "reuse" factorial generated
 def q53(upper_range=100, ncr_requirement=10 ** 6):
     fitting_ncr_combinations = 0
     r_boundary = 23 // 2 - 1
