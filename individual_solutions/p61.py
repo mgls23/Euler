fa1452820f860081e9fe6c70ce99363afb7c0e01
Taehyun Lee <taehyunlee12@gmail.com>
2020-03-05 21:40:51 +0000

    [61] Refactoring

--- individual_solutions/p61.py
+++ individual_solutions/p61.py
@@ -18,7 +18,7 @@ class PolynomialSequence:
     def to_index(number): pass
 
     @classmethod
-    def generate_in_range(cls, digit=4):
+    def generate_numbers_with(cls, digit):
         lower_bound = 10 ** (digit - 1)
         upper_bound = 10 ** digit
 
@@ -100,10 +100,6 @@ class Octagonal(PolynomialSequence):
         return ((3 * number + 1) ** 0.5 + 1) / 3
 
 
-class FoundCyclicalException(Exception):
-    pass
-
-
 class PolynomialNode:
     def __init__(self, polynomial_type, number):
         self.polynomial_type = polynomial_type
@@ -116,59 +112,56 @@ class PolynomialNode:
         self.connected = []
 
     def add_connection(self, node):
-        if self.suffix == node.prefix:
+        if self.is_connected(node):
             self.connected.append(node)
+
+    def is_connected(self, node):
+        return self.suffix == node.prefix
 
     def __str__(self):
         return f'[{self.polynomial_type}]({self.number})'
 
     def can_form_cycle_with(self, path):
-        if all(self.polynomial_type != node.polynomial_type for node in path):
-            if len(path) == 5:
-                if path[0].prefix == self.suffix:
-                    raise FoundCyclicalException
-                else:
-                    return False
-            return True
-        return False
+        return all(self.polynomial_type != node.polynomial_type for node in path)
 
 
-def break_down(polynomial_type, digits):
-    numbers = polynomial_type.generate_in_range(digits)
-
-    broken_down = []
-    for number in numbers:
-        broken_down.append(PolynomialNode(polynomial_type.polynomial_degree, number))
-
-    return broken_down
+def generate_polynomial_nodes(polynomial_type, digits):
+    return [
+        PolynomialNode(polynomial_type.polynomial_degree, number)
+        for number in polynomial_type.generate_numbers_with(digits)
+    ]
 
 
-def q61(digits=4):
-    triangular_numbers = break_down(TriangleNumbers, digits)
-    square_numbers = break_down(SquareNumbers, digits)
-    pentagonal_numbers = break_down(Pentagonal, digits)
-
-    hexagonal_numbers = break_down(Hexagonal, digits)
-    heptagonal_numbers = break_down(Heptagonal, digits)
-    octagonal_numbers = break_down(Octagonal, digits)
-
-    all_polynomials = [triangular_numbers, square_numbers, pentagonal_numbers, hexagonal_numbers,
-                       heptagonal_numbers, octagonal_numbers]
-
-    for collection1, collection2 in permutations(all_polynomials, 2):
+def connect_nodes_in_graph(graph):
+    for collection1, collection2 in permutations(graph, 2):
         for node1 in collection1:
             for node2 in collection2:
                 node1.add_connection(node2)
 
-    paths = deque([([node], node) for node in all_polynomials[0]])
+
+def find_cyclical_figurate_number(graph):
+    paths = deque([([node], node) for node in graph[0]])
     while paths:
         path, last_node_in_path = paths.pop()
         for connected_node in last_node_in_path.connected:
-            try:
-                if connected_node.can_form_cycle_with(path):
-                    paths.append((path + [connected_node], connected_node))
-            except FoundCyclicalException:
-                return sum([node.number for node in path + [connected_node]])
+            if connected_node.can_form_cycle_with(path):
+                if len(path) == 5:
+                    if connected_node.is_connected(path[0]):
+                        logging.debug([(node.polynomial_type, node.number) for node in path + [connected_node]])
+                        return sum([node.number for node in path + [connected_node]])
+
+                    continue
+
+                paths.append((path + [connected_node], connected_node))
+
+
+def q61(digits=4):
+    polynomial_types = [TriangleNumbers, SquareNumbers, Pentagonal, Hexagonal, Heptagonal, Octagonal]
+    all_polynomial_numbers_in_range = [generate_polynomial_nodes(polynomial_type, digits)
+                                       for polynomial_type in polynomial_types]
+
+    connect_nodes_in_graph(all_polynomial_numbers_in_range)
+    return find_cyclical_figurate_number(all_polynomial_numbers_in_range)
 
 
 if __name__ == '__main__':
