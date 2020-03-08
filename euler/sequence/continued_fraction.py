cb31554ab6fbc7d8b4ee735a224af2918e74423b
Taehyun Lee <taehyunlee12@gmail.com>
2020-03-08 19:31:16 +0000

    [65] Solution added

---
+++ euler/sequence/continued_fraction.py
@@ -0,0 +1,18 @@
+def square_root_2_generator(stop_iteration):
+    numerator, denominator = 1, 1
+    for _ in range(stop_iteration):
+        numerator += denominator * 2
+        denominator = numerator - denominator
+
+        yield numerator, denominator
+
+
+def general_iterator(number, periods):
+    # TODO :: something along this line
+    previous_numerator, previous_denominator = 1, 1
+    numerator, denominator = 1, 1
+    for period in periods:
+        numerator = previous_numerator + period * numerator
+        denominator = previous_denominator + period * denominator
+
+        yield numerator, denominator
--- individual_solutions/p57.py
+++ individual_solutions/p57.py
@@ -1,13 +1,5 @@
+from euler.sequence.continued_fraction import square_root_2_generator
 from euler.util.decorators import timed_function
-
-
-def square_root_2_generator(stop_iteration):
-    numerator, denominator = 1, 1
-    for _ in range(stop_iteration):
-        numerator += denominator * 2
-        denominator = numerator - denominator
-
-        yield numerator, denominator
 
 
 def numerator_has_more_digits(numerator, denominator):
