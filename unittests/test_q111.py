7fb7a1e207ac3a18d8e2e6ebf17b4eee8f931646
Taehyun Lee <taehyunlee12@gmail.com>
2022-08-27 19:29:55 +0100

    Tests + answersxy

--- unittests/test_q111.py
+++ unittests/test_q111.py
@@ -1,4 +1,4 @@
-from individual_solutions.q111 import primes_with_runs_m_1, find_primes_with_runs
+from individual_solutions.q111 import primes_with_runs_m_1, find_primes_with_runs, q111
 
 
 def test_function_m_example():
@@ -9,16 +9,32 @@ def test_function_m_example():
 
 
 def test_4_digit_sums_all():
-    assert [sum(find_primes_with_runs(d, digit=4)) for d in '0123456789'] == \
-           [
-               67061,
-               22275,
-               2221,
-               46214,
-               8888,
-               5557,
-               6661,
-               57863,
-               8887,
-               48073,
-           ]
+    assert [sum(find_primes_with_runs(d, digit=4)) for d in '0123456789'] == [
+        67061,
+        22275,
+        2221,
+        46214,
+        8888,
+        5557,
+        6661,
+        57863,
+        8887,
+        48073,
+    ]
+
+
+def test_q111():
+    assert [sum(find_primes_with_runs(d, digit=10)) for d in '0123456789'] == [
+        38000000042,
+        12882626601,
+        97447914665,
+        23234122821,
+        4444444447,
+        5555555557,
+        6666666661,
+        59950904793,
+        285769942206,
+        78455389922,
+    ]
+
+    assert q111() == 612407567715
