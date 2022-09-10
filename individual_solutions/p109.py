88a3ccb438670eb9b494a1cbb1d924590384392d
Taehyun Lee <taehyunlee12@gmail.com>
2022-09-10 14:53:38 +0100

    [109] Speedup - start with Doubles

--- individual_solutions/p109.py
+++ individual_solutions/p109.py
@@ -15,34 +15,29 @@ def dart_score_to_string(multiplier, score):
 
 def checkout(target_score):
 	valid_checkout_paths = set()
-	chosen_darts = []
 
-	def helper(remaining_score, last_score):
+	def helper(remaining_score):
 		if len(chosen_darts) == 3 or remaining_score <= 0:
-			if chosen_darts[-1][0] == 2 and remaining_score == 0:
-				combination = tuple(sorted(chosen_darts[:-1])) + (chosen_darts[-1],)
-				valid_checkout_paths.add(tuple(map(dart_score_tuple_to_string, combination)))
+			if remaining_score == 0:
+				checkout_path = tuple(sorted(chosen_darts[1:])) + (chosen_darts[0],)
+				valid_checkout_paths.add(checkout_path)
 
 			return
 
-		for score in range(SINGLE, 20 + 1):
-			for multiplier in range(SINGLE, TRIPLE + 1):
+		for score in list(range(1, 20 + 1)) + [BULLS_EYE]:
+			multiplier_range = range(SINGLE, TRIPLE + 1)
+			if score == BULLS_EYE: multiplier_range = range(SINGLE, DOUBLE + 1)
+			for multiplier in multiplier_range:
 				new_score = score * multiplier
-				if new_score < last_score and multiplier != DOUBLE: continue
 
 				chosen_darts.append((multiplier, score))
-				helper(remaining_score - new_score, new_score)
+				helper(remaining_score - new_score)
 				chosen_darts.pop()
 
-		for multiplier in range(SINGLE, DOUBLE + 1):
-			new_score = BULLS_EYE * multiplier
-			if new_score < last_score and multiplier != DOUBLE: continue
+	for double_score in list(range(1, 20 + 1)) + [BULLS_EYE]:
+		chosen_darts = [(DOUBLE, double_score)]
+		helper(target_score - DOUBLE * double_score)
 
-			chosen_darts.append((multiplier, BULLS_EYE))
-			helper(remaining_score - new_score, new_score)
-			chosen_darts.pop()
-
-	helper(target_score, 0)
 	return valid_checkout_paths
 
 
