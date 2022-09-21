e35fadf893407b7610db3afcab2d6a4b178500e6
Taehyun Lee <taehyunlee12@gmail.com>
2022-09-21 22:23:41 +0100

    Nearly there

--- individual_solutions/p205.py
+++ individual_solutions/p205.py
@@ -1,7 +1,60 @@
 import logging
+import random
+from itertools import accumulate
 
 from euler.maths.multiplications import lowest_common_multiple
 from euler.util.array import zero_padded
+
+
+def simple_case_2():
+	#    0  1  2  3  4  5  6  7  8
+	a = [0, 1, 1, 1, 0, 0, 0, 0, 2]
+	b = [0, 0, 0, 0, 1, 1, 1, 1, 1]
+
+	# b=4 [w=3/l=2]
+	# b=5 [w=3/l=2]
+	# b=6 [w=3/l=2]
+	# b=7 [w=3/l=2]
+	# b=8 [w=3/d=2]
+	# w=15
+
+	aa = list(accumulate(a))
+	b_win = 0
+	for i in range(1, len(a)):
+		b_win += b[i] * aa[i - 1]
+
+	print(b_win / 25)
+
+
+def simple_case():
+	#    0  1  2  3  4  5  6  7  8
+	a = [0, 1, 1, 1, 1, 1, 0, 0, 0]
+	b = [0, 0, 0, 0, 1, 1, 1, 1, 1]
+
+	aa = list(accumulate(a))
+	b_win = 0
+	for i in range(1, len(a)):
+		b_win += b[i] * aa[i - 1]
+
+	print(b_win / 25)
+
+
+def simulate_simpler_case():
+	a = [1, 2, 3, 4, 5]
+	b = [4, 5, 6, 7, 8]
+	# b's WR = 3/5 * 5/5 [b=6,7,8,a=*] + 1/5 * 4/5 [b=5,a=1,2,3,4] + 1/5 * 3/5 [b=4,a=1,2,3]
+	# (15 + 4 + 3) / 25
+	# = 22 / 25
+	# 0.88
+
+	b_wins = 0
+	simulation = 100000
+
+	for i in range(simulation):
+		an, bn = random.choice(a), random.choice(b)
+		if bn > an: b_wins += 1
+
+	print(b_wins / simulation)
 
 
 def lexicographical_ordering(number_of_dice, dice_max, dice_min=1):
@@ -71,10 +124,11 @@ def q205():
 	nine_4s_adjusted = list(map(lambda count: count * adjust_4, nine_4s))
 	six_6s_adjusted = list(map(lambda count: count * adjust_6, six_6s))
 
-	victory = sum(max(count - six_6s_adjusted[i], 0)
-	              for i, count in enumerate(nine_4s_adjusted))
+	six_6s_cumulate = list(accumulate(six_6s_adjusted))
+	nine_4s_victories = sum(value * six_6s_cumulate[i]
+	                        for i, value in enumerate(nine_4s_adjusted[1:]))
 
-	return victory / lcm
+	return nine_4s_victories / (lcm * lcm)
 
 
 if __name__ == '__main__':
