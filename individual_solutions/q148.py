153fda6a35ea0b4e1ba09d0741c0ea55cdca8ffc
Taehyun Lee <taehyunlee12@gmail.com>
2022-11-14 18:58:58 +0000

    wip

--- individual_solutions/q148.py
+++ individual_solutions/q148.py
@@ -1,11 +1,25 @@
 import logging
+from math import log
 
 
 def investigate_7s():
 	# observation 1: multiples of 7 follow the pattern of +6, +5, +4, +3... but with jumps
 	from scipy.linalg import pascal
-	for multiple in range(7, 500 + 1, 7):
+	for multiple in range(7, 200 + 1, 7):
 		print(multiple, len([i for i in pascal(multiple + 1, kind='lower')[-1] if i % 7 == 0]))
+
+	for multiple in range(49, 100 + 1):
+		print(multiple, len([i for i in pascal(multiple + 1, kind='lower')[-1] if i % 7 == 0]))
+
+
+def not_optimal_but_pretty_good(upper_bound):
+	powers_of_seven = [0] + [int(log(number, 7)) for number in range(1, upper_bound + 1)]
+
+	def cool_function(number):
+		sevens_in_number = powers_of_seven[number]
+		for n in range(0, number + 1):
+			r = number - n
+			
 
 
 def q148(number):
@@ -27,5 +41,5 @@ if __name__ == '__main__':
 
 	logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 
-	investigate_7s()
+	not_optimal_but_pretty_good(200)
 	assert (timed_function(q148)(100) == 2361)
